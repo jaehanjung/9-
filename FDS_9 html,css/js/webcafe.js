94ae32baf891a8c6ee54f0886eab9f5853a34328
jaehanjung <wjdwogks23@gmail.com>
2018-03-30 18:11:16 +0900

    03.30 main영역 작업

---
+++ FDS_9 html,css/js/webcafe.js
@@ -0,0 +1,6 @@
+let container = document.querySelector('.container');
+let grid = document.querySelector('.btn-grid');
+
+grid.addEventListener('click', function(){
+  container.classList.toggle('is-act')
+});
