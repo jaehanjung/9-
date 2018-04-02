4192c4a270252acd9bd72e03a531a846ea484872
jaehanjung <wjdwogks23@gmail.com>
2018-04-02 19:23:36 +0900

    news 영역 완료

--- FDS_9 html,css/js/webcafe.js
+++ FDS_9 html,css/js/webcafe.js
@@ -4,3 +4,13 @@ let grid = document.querySelector('.btn-grid');
 grid.addEventListener('click', function(){
   container.classList.toggle('is-act')
 });
+
+
+// 제이쿼리
+// 
+
+// var video = document.querySelector('.news-video');
+
+// video.addEventListener('mouseover', function(){
+//  console.log('avc')
+// })
