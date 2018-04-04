e4e2c677acd46d15e8f224b26725bfc0d924aaf2
jaehanjung <wjdwogks23@gmail.com>
2018-04-04 18:46:46 +0900

    tab-content연습용

--- FDS_9 html,css/js/webcafe.js
+++ FDS_9 html,css/js/webcafe.js
@@ -1,13 +1,35 @@
-let container = document.querySelector('.container');
-let grid = document.querySelector('.btn-grid');
+$(document).ready(function() {
+  var container = $(".container");
+  var grid = $(".btn-grid");
+  var video = $(".news-video");
 
-grid.addEventListener('click', function(){
-  container.classList.toggle('is-act')
+  // 자동재생할때 볼륨을 0으로
+  video.get(0).volume = 0.0;
+  // 마우스를 비디오에 대면 실행
+  video.on("mouseover focusin", function() {
+    // video[0].play();
+    this.volume = 1.0;
+  });
+  // 마우스를 비디오에서 띄면 멈춤
+  video.on("mouseout focusout", function() {
+    video[0].pause();
+    // this.volume = 0;
+  });
+
+  grid.click(function() {
+    container.toggleClass("is-act");
+  });
 });
 
+// let container = document.querySelector('.container');
+// let grid = document.querySelector('.btn-grid');
+
+// grid.addEventListener('click', function(){
+//   container.classList.toggle('is-act')
+// });
 
 // 제이쿼리
-// 
+//
 
 // var video = document.querySelector('.news-video');
 
