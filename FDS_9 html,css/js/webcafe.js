b67ad443c691287990368330b5ee20f37b3e4c05
jaehanjung <wjdwogks23@gmail.com>
2018-04-09 18:46:50 +0900

    web-cafe 완성

--- FDS_9 html,css/js/webcafe.js
+++ FDS_9 html,css/js/webcafe.js
@@ -1,38 +1,78 @@
-$(document).ready(function() {
-  var container = $(".container");
-  var grid = $(".btn-grid");
-  var video = $(".news-video");
+ let btn = document.querySelector(".btn-menubar");
+ let nav = document.querySelector('.navigation');
+ btn.addEventListener('click',function(){
+   nav.classList.toggle('is-act');
+  
+ })
+ 
+ 
+ // 자바스크립트를 이용한 버튼요소
+  const menuItems =  document.querySelectorAll('.menu>li');
+  for (let i = 0; i < menuItems.length; i++) {
+    menuItems[i].addEventListener('click', function() {
+    if(menuItems[i].classList.contains("menu-act")){
+      menuItems[i].classList.remove("menu-act");
+    } else {
+      for(let j = 0; j < menuItems.length; j++){
+        menuItems[j].classList.remove('menu-act');
+      }
+      menuItems[i].classList.add('menu-act');
+    }
+  })
+};
 
-  // 자동재생할때 볼륨을 0으로
-  video.get(0).volume = 0.0;
-  // 마우스를 비디오에 대면 실행
-  video.on("mouseover focusin", function() {
-    // video[0].play();
-    this.volume = 1.0;
-  });
-  // 마우스를 비디오에서 띄면 멈춤
-  video.on("mouseout focusout", function() {
-    video[0].pause();
-    // this.volume = 0;
-  });
+// $(document).ready(function () {
+//   var viewport = window.matchMedia('(max-width:999px)');
+//   if (viewport.matches) {
+//     var nav = $('.navigation');
+//     var btn = $('.btn-menubar');
+//     var menu = $('.menu');
+//     var menuLists = $('.menu-list');
+//     var menuItems = $('.menu-item');
+//     var subMenus = $('.sub-menu');
+//     var slogan = $('.slogan-heading')
 
-  grid.click(function() {
-    container.toggleClass("is-act");
-  });
-});
+//     // 999px 이하에서 적용할 코드
+//     menuItems.attr('aria-haspopup', 'true');
+//     menuItems.attr('aria-expanded', 'false');
+//     menuItems.attr('role', 'button');
+//     menuItems.attr('tabindex', '0');
+//     menuItems.addClass('icon-plus');
+//     slogan.addClass('a11y-hidden');
 
-// let container = document.querySelector('.container');
-// let grid = document.querySelector('.btn-grid');
+//     btn.on('click', function(e) {
+//       e.preventDefault();
+//       nav.toggleClass('is-act');
+//       if (nav.hasClass('is-act')) {
+//         btn.attr('aria-label', '메인 메뉴 닫기');
+//       } else {
+//         btn.attr('aria-label', '메인 메뉴 열기');
+//       }
+//     });
 
-// grid.addEventListener('click', function(){
-//   container.classList.toggle('is-act')
+//     menuItems.on('click keydown', function(e) {
+//       if (e.type === 'click' || (e.type === 'keydown' && e.keyCode === 13)) {
+//         menuLists.removeClass('menu-act');
+//         menuItems.addClass('icon-plus');
+//         subMenus.css('display', 'none');
+//         $(this).parent().addClass('menu-act');
+//         if ($(this).parent().hasClass('menu-act')) {
+//           $(this).attr('aria-expanded', 'true');
+//           $(this).addClass('icon-minus');
+//           $(this).removeClass('icon-plus');
+//           $(this).siblings().css('display', 'block');
+//         } else {
+//           $(this).attr('aria-expanded', 'false');
+//         }
+//       }
+//     });
+//     menu.on('focusout', function(){
+//       nav.removeClass('is-act');
+//     });
+//     menu.on('focusin', function(){
+//       nav.addClass('is-act');
+//     });
+//   } else {
+//     // 1000px 이상에서 사용할 코드
+//   }
 // });
-
-// 제이쿼리
-//
-
-// var video = document.querySelector('.news-video');
-
-// video.addEventListener('mouseover', function(){
-//  console.log('avc')
-// })
